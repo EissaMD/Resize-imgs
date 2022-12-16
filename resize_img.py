11b5620d6a7ce1cabe22d1620acf55b4ce78c98f
EissaMD <eissa.code@gmail.com>
2022-12-17 01:15:43 +0300

    textbox to show progress & add 'Re-size' button

--- resize_img.py
+++ resize_img.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import os
 from os import listdir
-import tkinter
+import tkinter as tk
 from tkinter.ttk import *
 
-class App(tkinter.Tk):
+class App(tk.Tk):
     WIDTH = 400
-    HEIGHT = 500
+    HEIGHT = 360
     def __init__(self):
         super().__init__()
         self.title("Resize Images")
@@ -14,27 +14,30 @@ class App(tkinter.Tk):
         self.minsize(App.WIDTH , App.HEIGHT)
         self.maxsize(App.WIDTH , App.HEIGHT)
         self.grid_columnconfigure(0, weight=1)
-        
+        # title
         Label(self,text="Resize Images" , font="Times 25 bold").grid(row=0, column=0, pady=20 )
-        
+        # ask for folder path
         path_frame = Frame(self)
         path_frame.grid(row=1, column=0 , sticky = "we")
         path_frame.columnconfigure((0,2),weight=1 )
         path_frame.columnconfigure(1,weight=10)
         Label(path_frame,text=f"File Path :" ).grid(row=0, column=0,  sticky = "we" , padx=(5,0) , pady=10 )
-        path_entry = Entry(path_frame,text="Click on 'Import' button")
+        path_entry = Entry(path_frame)
         path_entry.grid(row=0, column=1,sticky="we")
-
-        Button(path_frame, text="Import").grid(row=0, column=2 , padx=(0,5))
-        
+        Button(path_frame, text="Select Folder").grid(row=0, column=2 , padx=(0,5))
+        # ask for image dimensions
         dimensions_frame = Frame(self)
         dimensions_frame.grid(row=2, column=0 , sticky = "we")
         Label(dimensions_frame,text=f"Image size :  " ).pack(side="left")
-        entry_w = Entry(dimensions_frame,text="Click on 'Import' button" , width=10)
+        entry_w = Entry(dimensions_frame , width=10)
         entry_w.pack(side="left")
         Label(dimensions_frame,text=f"X" ).pack(side="left")
-        entry_h = Entry(dimensions_frame,text="Click on 'Import' button" , width=10)
+        entry_h = Entry(dimensions_frame , width=10)
         entry_h.pack(side="left")
+        # show image names in a text box
+        textbox = tk.Text(self , bg="gray90" , height=10)
+        textbox.grid(row=3, column=0 ,sticky="nswe" , pady=10, padx=10)
+        Button(self, text="Re-size").grid(row=4, column=0 )
         
 def resize():    
     imgs_path = r"."
